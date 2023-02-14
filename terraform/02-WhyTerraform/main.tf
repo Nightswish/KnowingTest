4e5115605f28916a1f5cacdac116d7d986a0f03a
jk.kim <luvrkdus@gmail.com>
2023-02-14 15:55:54 +0900

    단일서버 배포 예제

--- terraform/02-WhyTerraform/main.tf
+++ terraform/02-WhyTerraform/main.tf
@@ -1,3 +1,12 @@
 provider "aws" {
   region = "us-west-1"
-}  
+} 
+
+resource "aws_instance" "example" {
+  ami = "ami-0427c7b524bf024ed"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "terraform-example"
+  }
+} 
