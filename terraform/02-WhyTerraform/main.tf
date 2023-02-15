acc7230d6f6fc1d0bc7709612073ab03e93e2496
jk.kim <luvrkdus@gmail.com>
2023-02-15 23:31:00 +0900

    단일 웹 서버 배포

--- terraform/02-WhyTerraform/main.tf
+++ terraform/02-WhyTerraform/main.tf
@@ -5,8 +5,26 @@ provider "aws" {
 resource "aws_instance" "example" {
   ami = "ami-0427c7b524bf024ed"
   instance_type = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.instance.id]
+
+  user_data = <<-EOF
+                 #!/bin/bash
+                 echo "Hello, World" > index.html
+                 nohub busybox httpd -f -p 8080 &
+                 EOF
 
   tags = {
     Name = "terraform-example"
+  }
+}
+
+resource "aws_security_group" "instance" {
+  name = "terraform-exmaple-instance"
+
+  ingress {
+  from_port   = 8080
+  to_port     = 8080
+  protocol    = "tcp"
+  cidr_blocks = [ "0.0.0.0/0" ]
   }
 } 
