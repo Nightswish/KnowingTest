6f90c39b0e80dffb643ba6d54c396a0e3c5d794d
jk.kim <luvrkdus@gmail.com>
2023-02-23 16:03:51 +0900

    AWS S3 bucket을 이용한 상태 파일 공유

---
+++ terraform/03-TerraformState/01.statefile_withS3bucket/main.tf
@@ -0,0 +1,49 @@
+terraform {
+  required_version = ">= 1.0.0, < 2.0.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-west-1"
+} 
+
+resource "aws_s3_bucket" "terraform_state" {
+
+  bucket = var.bucket_name
+
+  // This is only here so we can destroy the bucket as part of automated tests. You should not copy this for production
+  // usage
+  force_destroy = true
+
+  # 상태 파일의 이력관리를 위해
+  # 버전관리 기능을 활성화 합니다.
+  versioning {
+    enabled = true
+  }
+
+  # Enable server-side encryption by default
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+}
+
+resource "aws_dynamodb_table" "terraform_locks" {
+  name         = var.table_name
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
